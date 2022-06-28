cf8d91140473e2a2f861c1c5d7a42d7b9de9e4ce
Jose Reyes <jreyesri@cbu.edu>
2022-06-28 16:32:26 -0400

    Started on task 3

--- contacts/models.py
+++ contacts/models.py
@@ -8,6 +8,9 @@ class Note(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     # author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.text
+
 
 class Contact(models.Model):
     phone_regex = RegexValidator(
